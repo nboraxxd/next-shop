bf33e54696b7a985d2945542c38e78da5a5d8384
bora <nhatbaoxxd@gmail.com>
2024-05-18 20:47:34 +0700

    Using local fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <main>
-      <h1 className='text-3xl font-semibold'>Home</h1>
+      <h1 className="text-3xl">Home</h1>
+      <p className="font-arimo text-lg">Welcome to your new Next.js site!</p>
+      <p className="font-nunito text-lg">This is a starter template for your new project.</p>
       <Button>Click me</Button>
     </main>
   )
