a1eff1b10ed51fd3ea96784512bc42aabe403272
bora <nhatbaoxxd@gmail.com>
2024-05-20 21:55:41 +0700

    Add registration page and form

--- src/app/(logged-out)/register/page.tsx
+++ src/app/(logged-out)/register/page.tsx
@@ -1,13 +1,17 @@
 import Link from 'next/link'
 
-export default function Register() {
+import RegisterForm from '@/components/form/register-form'
+
+export default function RegisterPage() {
   return (
     <>
       <h1 className="flex items-center gap-2 text-2xl font-semibold">Register with Next free</h1>
       <p>Register to access the best course to learn Next.js</p>
+
       <div className="mt-5 flex w-full max-w-96 flex-col">
-        [[form will be here]]
-        <div className="mt-2 flex items-center justify-center gap-1">
+        <RegisterForm />
+
+        <div className="mt-4 flex items-center justify-center gap-1">
           <span>Already have an account?</span>
           <Link href="/login" className="text-blue-600 hover:underline">
             Login
---
+++ src/components/form/register-form.tsx
@@ -0,0 +1,115 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+import { RegisterSchema, RegisterSchemaType } from '@/lib/schemaValidations/auth.schema'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import envConfig from '@/constants/config'
+
+export default function RegisterForm() {
+  const form = useForm<RegisterSchemaType>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  })
+
+  async function onValid(values: RegisterSchemaType) {
+    const result = await fetch(`${envConfig.NEXT_PUBLIC_API_ENDPOINT}/auth/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(values),
+    }).then((res) => res.json())
+
+    console.log('🔥 ~ onValid ~ result:', result)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onValid)} noValidate className="flex w-full flex-col gap-3.5">
+        {/* Name */}
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="space-y-1.5">
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="Bruce Wayne" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Email */}
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="space-y-1.5">
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" autoComplete="email" placeholder="bruce@wayne.dc" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Password */}
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem className="space-y-1.5">
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  autoComplete="new-password"
+                  placeholder="Please enter your password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Confirm password */}
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem className="space-y-1.5">
+              <FormLabel>Cofirm password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  autoComplete="new-password"
+                  placeholder="Please confirm your password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Button */}
+        <Button type="submit" className="mt-2">
+          Register
+        </Button>
+      </form>
+    </Form>
+  )
+}
