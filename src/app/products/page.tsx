063ff1656a37be9d9da5baeb91ac9270350cae52
bora <nhatbaoxxd@gmail.com>
2024-06-01 20:50:59 +0700

    middleware for product paths

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,19 +1,24 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { compareDesc, parseISO } from 'date-fns'
 
-import { Button } from '@/components/ui/button'
 import productApi from '@/api-requests/product.api'
-import { DelProductButton } from '@/components/shared'
+import { Button } from '@/components/ui/button'
+import { DelProductButton, Heading } from '@/components/shared'
 
 export default async function ProductsPage() {
-  const products = await productApi.getProducts()
+  const response = await productApi.getProducts()
+
+  const sortedProducts = [...response.payload.data].sort((a, b) =>
+    compareDesc(parseISO(a.updatedAt), parseISO(b.updatedAt))
+  )
 
   return (
     <div>
-      <h1>Product List</h1>
-      <div className="space-y-5">
-        {products.payload.data.map((product, index) => (
-          <div key={product.id} className="flex space-x-4">
+      <Heading>Product List</Heading>
+      <div className="mx-auto mt-8 max-w-2xl space-y-7">
+        {sortedProducts.map((product, index) => (
+          <div key={product.id} className="flex space-x-5">
             <Image
               src={product.image}
               alt={product.name}
@@ -22,17 +27,21 @@ export default async function ProductsPage() {
               className="size-32 object-cover"
               priority={index < 7}
             />
-            <h3>{product.name}</h3>
-            <div>{product.price}</div>
-            <div className="flex space-x-2">
-              <Button variant={'outline'} asChild>
-                <Link href={`/products/${product.id}`}>View</Link>
-              </Button>
-              <Button variant={'outline'} asChild>
-                <Link href={`/products/${product.id}/update`}>Update</Link>
-              </Button>
+            <div className="flex flex-col gap-6">
+              <div className="flex gap-3">
+                <h3>Name: {product.name}</h3>
+                <p>Price: {product.price}</p>
+              </div>
+              <div className="flex space-x-2">
+                <Button variant={'outline'} asChild>
+                  <Link href={`/products/${product.id}`}>View</Link>
+                </Button>
 
-              <DelProductButton productId={product.id} />
+                <Button variant={'outline'} asChild>
+                  <Link href={`/products/edit/${product.id}`}>Edit</Link>
+                </Button>
+                <DelProductButton productId={product.id} />
+              </div>
             </div>
           </div>
         ))}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const privatePaths = ['/me', '/me/update', '/profile']
-const loggedInPaths = ['/login', '/register']
+const loggedInPaths = ['/me', '/me/update', '/profile', '/products/add']
+const loggedOutPaths = ['/login', '/register']
+const editProductRegex = /^\/products\/edit\/[^/]+$/
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
@@ -10,14 +11,18 @@ export function middleware(request: NextRequest) {
 
   const sessionToken = request.cookies.get('sessionToken')?.value
 
-  if (privatePaths.some((privatePath) => pathname.startsWith(privatePath) && !sessionToken)) {
+  if (
+    (editProductRegex.test(pathname) || loggedInPaths.some((privatePath) => pathname.startsWith(privatePath))) &&
+    !sessionToken
+  ) {
+    console.log('🔥 ~ middleware ~ pathname:', pathname)
     const loginUrl = new URL('/login', request.url)
     loginUrl.searchParams.set('next', pathname)
 
     return NextResponse.redirect(loginUrl)
   }
 
-  if (loggedInPaths.some((loggedInPath) => pathname.startsWith(loggedInPath) && sessionToken)) {
+  if (loggedOutPaths.some((loggedInPath) => pathname.startsWith(loggedInPath) && sessionToken)) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
@@ -26,5 +31,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/me', '/me/update', '/profile', '/login', '/register'],
+  matcher: ['/me', '/me/update', '/profile', '/login', '/register', '/products/add', '/products/edit/:path*'],
 }
